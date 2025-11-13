7c6ec0ca6859dd4d8d8c1e8b2c567e1c12c4ed91
Steven Lau <github@stevenlau.net>
2025-11-13 10:36:39 +0000

    [githubId6] Fix createBook test to remove hardcoded titles

--- src/tests/book.spec.js
+++ src/tests/book.spec.js
@@ -2,15 +2,12 @@ const { faker } = require('@faker-js/faker')
 const { randomUUID } = require('crypto')
 const { spec } = require('pactum')
 
-//const randomContent = faker.lorem.sentences(2)
-
 url = 'http://books.tc.staging.catapult.com:4000/graphql'
-const randomTitle = faker.book.randomTitle
 
 
 describe('Query book tests', () => {
 
-    test('Test query all books and then query individual book by id', async () => {
+    test('Test query all books and then query first individual book by id', async () => {
         await spec()
         .post(url)
         .withGraphQLQuery(`
@@ -45,25 +42,26 @@ describe('Query book tests', () => {
 
 describe('Create book test', () => {
 
-    //const randomTitle = faker.book.randomTitle
-    const title = "Great Expectations"
-
-    // TODO fix: pactumjs withGraphQLVariables not passing through values
-    // fix for dynamic books from Faker
     test('Test create a new book and query by id', async () => {
+
+        const randomTitle = faker.book.title()
+
         await spec()
         .post(url)
         .withGraphQLQuery(`
-            mutation {
-                createBook(createBookInput: { title: "Great Expectations" }) {
+            mutation createBook($title: String!) {
+                createBook(createBookInput: { title: $title }) {
                     id
                     title
                 }
             }
         `)
+        .withGraphQLVariables({
+            "title": randomTitle
+        })
         .expectHeader('content-type', 'application/json; charset=utf-8')
         .expectStatus(200)
-        .expectJsonMatch('data.createBook.title', 'Great Expectations')
+        .expectJsonMatch('data.createBook.title', randomTitle)
         .stores('bookId', 'data.createBook.id')
 
         await spec()
@@ -86,25 +84,26 @@ describe('Create book test', () => {
 
 describe('Remove book tests', () => {
 
-    //const randomTitle = faker.book.randomTitle
-    const title = "Great Expectations"
-
-    // TODO fix: pactumjs withGraphQLVariables not passing through values
-    // fix for dynamic books from Faker
     test('Test create a new book and remove and query', async () => {
+
+        const randomTitle = faker.book.title()
+
         await spec()
         .post(url)
         .withGraphQLQuery(`
-            mutation {
-                createBook(createBookInput: { title: "Great Expectations two" }) {
+            mutation createBook($title: String!) {
+                createBook(createBookInput: { title: $title }) {
                     id
                     title
                 }
             }
         `)
+        .withGraphQLVariables({
+            "title": randomTitle
+        })
         .expectHeader('content-type', 'application/json; charset=utf-8')
         .expectStatus(200)
-        .expectJsonMatch('data.createBook.title', 'Great Expectations two')
+        .expectJsonMatch('data.createBook.title', randomTitle)
         .stores('bookId', 'data.createBook.id')
 
         await spec()
@@ -188,33 +187,43 @@ describe('Remove book tests', () => {
 describe('Update book test', () => {
     
     test('Test create book and then update title', async () => {
+
+        const randomTitle = faker.book.title()
+        const updatedTitle = faker.book.title()
+
         await spec()
         .post(url)
         .withGraphQLQuery(`
-            mutation createBook {
-                createBook(createBookInput: { title: "Great Expectations" }) {
+            mutation createBook($title: String!) {
+                createBook(createBookInput: { title: $title }) {
                     id
                     title
                 }
             }
         `)
+        .withGraphQLVariables({
+            "title": randomTitle
+        })
         .expectHeader('content-type', 'application/json; charset=utf-8')
         .expectStatus(200)
-        .expectJsonMatch('data.createBook.title', 'Great Expectations')
+        .expectJsonMatch('data.createBook.title', randomTitle)
         .stores('bookId', 'data.createBook.id')
 
         await spec()
         .post(url)
         .withGraphQLQuery(`
-            mutation updateBook {
-                updateBook(updateBookInput: { id: { set: "$S{bookId}" }, title: { set: "Bleak House" }}) {
+            mutation updateBook($title: String!) {
+                updateBook(updateBookInput: { id: { set: "$S{bookId}" }, title: { set: $title }}) {
                     id
                     title
                 }
             }
         `)
+        .withGraphQLVariables({
+            "title": updatedTitle
+        })
         .expectHeader('content-type', 'application/json; charset=utf-8')
-        .expectJsonMatch('data.updateBook.title', 'Bleak House')
+        .expectJsonMatch('data.updateBook.title', updatedTitle)
         .stores('bookId', 'data.createBook.id')
 
         await spec()
@@ -229,7 +238,7 @@ describe('Update book test', () => {
         `)
         .expectStatus(200)
         .expectHeader('content-type', 'application/json; charset=utf-8')
-        .expectJsonMatch('data.updateBook.title', 'Bleak House')
+        .expectJsonMatch('data.updateBook.title', newRandomTitle)
     })
 })
 
